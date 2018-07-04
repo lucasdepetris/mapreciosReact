7db4123c704033f31a4c23a34d099c9a745448f0
mac <mac@macs-MacBook-Pro.local>
2018-07-04 20:05:33 -0300

    first commit maprecios web

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {Switch,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import {NotFound} from './pages/NotFound'
+import './App.css';
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Switch>
+        <Route exact path = '/' />
+        <Route component = {NotFound} />
+      </Switch>
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Header = () => (
+    <section className="hero is-medium is-primary is-bold">
+        <div className="hero-body">
+            <div className="container">
+                <h1 className="title">
+                Primary bold title
+                </h1>
+                <h2 className="subtitle">
+                Primary bold subtitle
+                </h2>
+            </div>
+        </div>
+    </section>
+)
