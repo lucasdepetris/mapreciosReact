3326a64dd8ee3be29abc61a274d7704896e92631
mac <mac@macs-MacBook-Pro.local>
2018-07-09 00:41:09 -0300

    - DiagonalView Div

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,8 @@
 import React from 'react'
 
 export const Header = () => (
-    <section className="hero is-medium is-info is-bold">
-        <div className="hero-body">
-            <div className="container">
-                <h1 className="title">
-                Maprecios
-                </h1>
-                <h2 className="subtitle">
-                Compra Mejor..
-                </h2>
-            </div>
-        </div>
-    </section>
+    <div className="quote">
+        <h1 className='title'>Maprecios</h1>
+        <p className='subtitle'>Compra mejor..</p>
+    </div>
 )
